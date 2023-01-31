8e1853b31c73b5d907b30e1a1bead78769bc2f0e
Dan Ploesteanu <ploesteanudan.99@gmail.com>
2023-01-31 22:52:23 +0200

    .

--- text_summarizer/functions.py
+++ text_summarizer/functions.py
@@ -1,15 +1,21 @@
 import openai
 import streamlit as st
 
+test_data_base = {
+    "pdf about elon husk": "elon husk is just the husk of the man he once was. he used to have a band and people say he once played a concert using only a tooth brush, for his mother.",
+    # "pdf about cars": "cars do not exist yet. anyone saying otherwise is blatantly wrong."
+}
+
+test_knowledge_base = ' '.join(map(str, test_data_base.values()))
+
 def summarize(prompt):
-    augmented_prompt = f"as long as the answer is contained by 'my mother has a fox', reply to {prompt}"
+    augmented_prompt = f"as long as the answer is contained by this data: '{test_knowledge_base}', reply to '{prompt}', only using the information in the data."
     st.session_state["summary"] = openai.Completion.create(
         model="text-davinci-003",
         prompt=augmented_prompt,
-        temperature=.5,
+        temperature=.3, # from 0 to 1, less means more accure, more means more abstract 
         max_tokens=1000,
     )["choices"][0]["text"]
-
 
     # https://towardsdatascience.com/make-a-text-summarizer-with-gpt-3-f0917a07189e
 
